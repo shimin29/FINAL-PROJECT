e1ec0bdacb75b0552d632c1d86dc3c85041b3134
SHI MIN <shimin.thor@gmail.com>
2026-06-23 14:03:33 +0800

    submit

--- add-favorite.php
+++ add-favorite.php
@@ -11,9 +11,7 @@ $db = new PDO("mysql:host=localhost;dbname=final_project", "root", "");
 $user_id = $_SESSION['user_id'];
 $game_id = $_GET['game_id'];
 
-$query = "SELECT * FROM favorites
-          WHERE user_id = :user_id
-          AND game_id = :game_id";
+$query = "SELECT * FROM favorites  WHERE user_id = :user_id  AND game_id = :game_id";
 
 $stmt = $db->prepare($query);
 $stmt->execute([
